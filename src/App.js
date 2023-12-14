ba807f0b81bdeb82977c2fcdc4b6b9a2b980cf66
MacKenzie <mjs94080@gmail.com>
2023-12-14 09:52:48 -0800

    3rd commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
