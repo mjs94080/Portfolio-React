594c838ca9e600b306284f59825ae1a3db018192
MacKenzie <mjs94080@gmail.com>
2023-12-14 11:07:38 -0800

    10th commit
    changed name for navbar in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+            MacKenzie Santiago
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
