89cd1d94e7aae7334f789882713e8e540d73f5ac
MacKenzie <mjs94080@gmail.com>
2023-12-14 10:45:59 -0800

    7th commit
    changed quotes in data.js, line 38

--- src/data.js
+++ src/data.js
@@ -35,8 +35,7 @@ export const projects = [
 
 export const testimonials = [
   {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    quote: "This fool likes to box and beat fools down.",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
     name: "Jesse Hicks",
     company: "Zoozle",
