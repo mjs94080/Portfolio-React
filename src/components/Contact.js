06dbecb4786fcad1c99bbbd83e19d728ed0ea4fc
MacKenzie <mjs94080@gmail.com>
2023-12-14 11:03:39 -0800

    9th commit
    changed contact info on Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,8 +45,8 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                97 Linda Mar Beach. <br />
+                Linda Mar, CA 94044
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,12 +54,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                mjs94080@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">213-373-4253</p>
             </div>
           </div>
         </div>
